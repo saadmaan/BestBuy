dcb03a43e4d5aace25bf0ee341e4d8b2f40d8079
niksust <niteshgik@gmail.com>
2018-01-26 03:07:14 +0600

    Add files via upload

---
+++ src/main/java/com/shopping/util/KeyGenerator.java
@@ -0,0 +1,16 @@
+package com.spring.shopping.util;
+
+import java.util.UUID;
+
+/*********************************
+ * This class is used to generate unique
+ * and random keys. Used in Account Activation
+ *
+ *******************************/
+public class KeyGenerator {
+	public static long generateKey(){
+		UUID uuidKey= UUID.randomUUID();
+		long key = uuidKey.getMostSignificantBits();
+		return key;
+	}
+}
